3e37bd7a35510bc2218650e9c4bc182d43daad27
RobertVida <weedofdeath@yahoo.com>
2016-04-29 23:58:44 +0300

    Grades page [DONE]

---
+++ db_grades.js
@@ -0,0 +1,52 @@
+var db_grades = [
+	{
+		id : 1,
+		years : [
+			{
+				id : "2014-2015",
+				semester1 : [
+					{name : "Course1", credits : 10, grade : 9},
+					{name : "Course2", credits : 9, grade : 4},
+					{name : "Course3", credits : 4, grade : 10}
+				],
+				semester2 : [
+					{name : "Course4", credits : 10, grade : 9},
+					{name : "Course5", credits : 9, grade : 4},
+					{name : "Course6", credits : 9, grade : 4}
+				]
+			},
+			{
+				id : "2015-2016",
+				semester1 : [
+					{name : "Course7", credits : 10, grade : 9},
+					{name : "Course8", credits : 9, grade : 10},
+					{name : "Course9", credits : 4, grade : 10}
+				],
+				semester2 : [
+					{name : "Course10", credits : 10, grade : 9},
+					{name : "Course11", credits : 9, grade : 6},
+					{name : "Course12", credits : 8, grade : 7},
+					{name : "Course13", credits : 4, grade : 4}
+				]
+			}			
+		]
+	},
+	{
+		id : 2,
+		years : [
+			{
+				id : "2015-2016",
+				semester1 : [
+					{name : "Course7", credits : 10, grade : 9},
+					{name : "Course8", credits : 9, grade : 10},
+					{name : "Course9", credits : 4, grade : 10}
+				],
+				semester2 : [
+					{name : "Course10", credits : 10, grade : 9},
+					{name : "Course11", credits : 9, grade : 6},
+					{name : "Course12", credits : 8, grade : 7}
+				]
+			}			
+		]
+	}
+]
