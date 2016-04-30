efa63bdc0752afa45b605902ed9a68a6d3585eef
RobertVida <weedofdeath@yahoo.com>
2016-04-30 03:45:19 +0300

    Email Page [DONE]

---
+++ db_emails.js
@@ -0,0 +1,78 @@
+var db_emails = [
+	{
+		id : 1,
+		folders : [
+			{
+				name : "inbox",
+				emails : [
+					{
+						id : 1, 
+						from : "otheruser@domain",
+						to : "user1@domain",
+						subject : "Example Mail 1",
+						cc : "ccuser@domain",
+						bcc : "bccuser@domain",
+						date : "10.04.2016",
+						content : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ut ante lectus. Nam et bibendum nisl, in malesuada sem. Nullam non ex non lectus fringilla vehicula. Donec rhoncus placerat pretium. Quisque varius aliquam nulla. Ut sem lectus, ultricies at ullamcorper in, tempor vulputate arcu. Nulla euismod enim id tortor maximus, feugiat convallis mi lacinia. Sed condimentum gravida commodo. Sed elementum, sem ac lobortis dictum, orci quam sollicitudin enim, eu consequat velit orci non est. Cras interdum odio sem, vehicula feugiat erat pharetra in. In vitae pretium justo. In pulvinar ipsum vitae neque tempus, sit amet tempus risus eleifend. Aenean et dignissim velit. Quisque mollis feugiat consequat. Mauris laoreet ante vel diam finibus, in feugiat ligula scelerisque."
+					},
+					{
+						id : 2,
+						from : "yetanotheruser@domain",
+						to : "user1@domain",
+						subject : "Example Mail 2",
+						cc : "",
+						bcc : "",
+						date : "02.02.2016",
+						content : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam non ex non lectus fringilla vehicula. Donec rhoncus placerat pretium. Quisque varius aliquam nulla. Ut sem lectus, ultricies at ullamcorper in, tempor vulputate arcu. Nulla euismod enim id tortor maximus, feugiat convallis mi lacinia. Sed condimentum gravida commodo. Sed elementum, sem ac lobortis dictum, orci quam sollicitudin enim, eu consequat velit orci non est. Cras interdum odio sem, vehicula feugiat erat pharetra in. In vitae pretium justo. In pulvinar ipsum vitae neque tempus, sit amet tempus risus eleifend. Aenean et dignissim velit."
+					},
+					{
+						id : 3,
+						from : "yetanotheruser@domain",
+						to : "user1@domain",
+						subject : "Example Mail 3",
+						cc : "",
+						bcc : "",
+						date : "02.01.2016",
+						content : "Ut sem lectus, ultricies at ullamcorper in, tempor vulputate arcu. Nulla euismod enim id tortor maximus, feugiat convallis mi lacinia. In vitae pretium justo. In pulvinar ipsum vitae neque tempus, sit amet tempus risus eleifend. Aenean et dignissim velit."
+					}
+				]
+			},
+			{
+				name : "sent",
+				emails : []
+			},
+			{
+				name : "trash",
+				emails : []
+			}
+		]
+	},
+	{
+		id : 2,
+		folders : [
+			{
+				name : "inbox",
+				emails : [
+					{
+						id : 4,
+						from : "otheruser@domain",
+						to :  "user2@domain",
+						subject : "Example Mail 1",
+						cc : "ccuser@domain",
+						bcc : "bccuser@domain",
+						date : "10.04.2016",
+						content : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ut ante lectus. Nam et bibendum nisl, in malesuada sem. Nullam non ex non lectus fringilla vehicula. Donec rhoncus placerat pretium. Quisque varius aliquam nulla. Ut sem lectus, ultricies at ullamcorper in, tempor vulputate arcu. Nulla euismod enim id tortor maximus, feugiat convallis mi lacinia. Sed condimentum gravida commodo. Sed elementum, sem ac lobortis dictum, orci quam sollicitudin enim, eu consequat velit orci non est. Cras interdum odio sem, vehicula feugiat erat pharetra in. In vitae pretium justo. In pulvinar ipsum vitae neque tempus, sit amet tempus risus eleifend. Aenean et dignissim velit. Quisque mollis feugiat consequat. Mauris laoreet ante vel diam finibus, in feugiat ligula scelerisque."
+					}
+				]
+			},
+			{
+				name : "sent",
+				emails : []
+			},
+			{
+				name : "trash",
+				emails : []
+			}
+		]
+	}
+]
