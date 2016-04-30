bf0ab783f2266a95715bfba563dc3e0aabc06bcd
RobertVida <weedofdeath@yahoo.com>
2016-04-30 05:37:02 +0300

    Schedule Page [DONE]

---
+++ db_schedule.js
@@ -0,0 +1,19 @@
+var db_schedule = 
+	{
+		scheduleA : [
+			[null, {name:"Course1 (Lab)", id:1}, null, null, null],
+			[null, null, null, null, null],
+			[null, {name:"Course1 (Course)", id:1}, {name:"Course1 (Seminar)", id:1}, null, null],
+			[null, null, null, null, null],
+			[{name:"Course2 (Course)", id:2}, null, null, {name:"Course2 (Lab)", id:2}, null],
+			[{name:"Course2 (Seminar)", id:2}, null, null, null, null]
+		],
+		scheduleB : [
+			[{name:"Course3 (Lab)", id:3}, null, null, null, null],
+			[{name:"Course3 (Seminar)", id:3}, null, null, null, null],
+			[{name:"Course3 (Course)", id:3}, null, null, null, null],
+			[null, null, null, null, null],
+			[null, null, null, null, null],
+			[null, null, null, null, null]
+		]
+	}
